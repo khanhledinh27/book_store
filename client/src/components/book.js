7aa37cf0412aff8edd42be10491e3d4425ddc87d
Khanh Le <161584352+khanhledinh27@users.noreply.github.com>
2024-10-05 21:49:19 +0700

    Update book.js

--- client/src/components/book.js
+++ client/src/components/book.js
@@ -19,7 +19,7 @@ const Book = () => {
     useEffect(() => {
         const fetchBooks = async () => {
             try {
-                const response = await fetch('http://localhost:5000/books');
+                const response = await fetch('https://localhost:5000/books');
                 if (!response.ok) {
                     throw new Error('Failed to fetch books');
                 }
